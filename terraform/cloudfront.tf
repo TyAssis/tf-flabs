dc0647f752541250d83eea86677eff01ce11f73b
TyAssis <tyassis3@gmail.com>
2026-07-13 03:10:19 -0300

    fix: gh ref

--- terraform/cloudfront.tf
+++ terraform/cloudfront.tf
@@ -11,7 +11,7 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
   default_cache_behavior {
     allowed_methods  = ["GET", "HEAD", "OPTIONS"]
     cached_methods   = ["GET", "HEAD"]
-    target_origin_id = local.app_name
+    target_origin_id = "${local.app_name}-s3-origin"
 
     viewer_protocol_policy = "allow-all"
     min_ttl                = 0
